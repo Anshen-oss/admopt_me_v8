d33af5d8709b921458b050c207475ce7c605ab65
anshen <plouisin@gmail.com>
2022-12-13 14:50:24 +0100

    effects

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Pet from "./Pet";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptiles"];
 
@@ -6,11 +7,31 @@ const SearchParams = () => {
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
+  const [pets, setPets] = useState([]);
   const breeds = ["Poodle"];
+
+  useEffect(() => {
+    requestPets();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+    const json = await res.json();
+
+    setPets(json.pets);
+  }
 
   return (
     <div className="search-params">
-      <form>
+      <form
+       onSubmit={(e) => {
+        e.preventDefault();
+        requestPets();
+       }}
+      >
         <label htmlFor="location">
           Location
           <input
@@ -45,7 +66,7 @@ const SearchParams = () => {
             value={breed}
             onChange={(e) => setBreed(e.target.value)}
           >
-            <option/>
+            <option />
             {breeds.map((breed) => (
               <option key={breed}>{breed}</option>
             ))}
@@ -54,6 +75,9 @@ const SearchParams = () => {
 
         <button>Submit</button>
       </form>
+      {pets.map((pet) => (
+        <Pet name={pet.name} animal={pet.animal} breed={pet.breed} key={pet.id} />
+      ))}
     </div>
   );
 };
